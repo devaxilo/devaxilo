747f8175e03b021069af66de052ce47c9d8c108d
MyPC <MyPC@MAYTINH-HGB75JB>
2017-12-16 10:14:48 +0700

    Remove un-used files

--- DevaxiloS/DevaxiloS.Services/Class1.cs
+++ DevaxiloS/DevaxiloS.Services/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevaxiloS.Services
+﻿namespace DevaxiloS.Services
 {
     public class Class1
     {
