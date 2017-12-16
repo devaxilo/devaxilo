26c6a1271dc6a8d449241ea2cf861c09844f6615
MyPC <MyPC@MAYTINH-HGB75JB>
2017-12-16 10:04:20 +0700

    Add project structure

---
+++ DevaxiloS/DevaxiloS.Web.FrontEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevaxiloS.Web.FrontEnd.Startup))]
+namespace DevaxiloS.Web.FrontEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
